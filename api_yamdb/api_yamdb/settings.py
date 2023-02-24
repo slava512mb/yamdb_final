367c34529f0ec4797622f0408493803951cdf965
Вячеслав Поликарский <polikarskij@yandex.ru>
2023-02-24 17:56:27 +0300

    commit 2.3 24.02.23

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -62,8 +62,8 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'NAME': os.getenv('DB_NAME', default='secret'),
+        'USER': os.getenv('POSTGRES_USER', default='secret'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='secret'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
